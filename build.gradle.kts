02b57dbb6e5463d619b8f3990d96c279a4b64354
Braden <bradenk04@proton.me>
2025-08-03 10:52:10 -0700

    fix: Changed spigot to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.21.5-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.21.8-R0.1-SNAPSHOT")
 }
 
 tasks.test {
