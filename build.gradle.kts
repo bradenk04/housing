87612a222069c1d08e24608c4fbbda76a1bc2046
Braden <bradenk04@proton.me>
2025-08-03 11:18:19 -0700

    feat: Added Lamp for commands & shadowJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 plugins {
-    kotlin("jvm") version "2.2.0"
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.shadow)
 }
 
 group = "io.github.bradenk04"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -13,9 +16,38 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.21.8-R0.1-SNAPSHOT")
+    // Main APIs:
+    compileOnly(libs.spigot.api)
+
+    // Command Frameworks:
+    implementation(libs.lamp.common)
+    implementation(libs.lamp.bukkit)
+    implementation(libs.lamp.brigadier)
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    withType<JavaCompile> {
+        options.compilerArgs.add("-parameters")
+    }
+
+    withType<KotlinJvmCompile> {
+        compilerOptions {
+            javaParameters = true
+        }
+    }
+
+    processResources {
+        filesMatching("plugin.yml") {
+            expand("version" to project.version)
+        }
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
+        relocate("io.github.revxrsal", "io.github.bradenk04.shaded.lamp")
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
 }
