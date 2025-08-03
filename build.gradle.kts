8c09baa8a494b21994b3c80c70e36da38be8b144
Braden <bradenk04@proton.me>
2025-08-03 10:51:36 -0700

    feat: Added spigot API

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://oss.sonatype.org/content/repositories/central")
 }
 
 dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.21.5-R0.1-SNAPSHOT")
 }
 
 tasks.test {
