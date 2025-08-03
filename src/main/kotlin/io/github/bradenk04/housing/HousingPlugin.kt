141f5d2b6870f90c81f48382e36aac1faf1ff0e2
Braden <bradenk04@proton.me>
2025-08-03 10:55:35 -0700

    feat: Main plugin file

---
+++ src/main/kotlin/io/github/bradenk04/housing/HousingPlugin.kt
@@ -0,0 +1,12 @@
+package io.github.bradenk04.housing
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class HousingPlugin : JavaPlugin() {
+    override fun onEnable() {
+
+    }
+    override fun onDisable() {
+
+    }
+}
